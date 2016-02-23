dfc919e88a042117cc2a2629a6ebbfc2877c8879
Scott <syhlion@gmail.com>
2016-02-23 13:52:08 +0800

    parse msgtype

--- client.go
+++ client.go
@@ -36,15 +36,15 @@ func (c *client) readPump() <-chan error {
 		c.ws.SetReadDeadline(time.Now().Add(pongWait))
 		c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 		for {
-			_, data, err := c.ws.ReadMessage()
+			msgType, data, err := c.ws.ReadMessage()
 			if err != nil {
 				errChan <- err
 				close(errChan)
 				return
 			}
-			/*if msgType != websocket.TextMessage {
+			if msgType != websocket.TextMessage {
 				continue
-			}*/
+			}
 
 			err = c.AfterReadStream(c, data)
 			if err != nil {
